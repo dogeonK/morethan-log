098ec5f0e9ceaa3a3ce3a38de5f888a16057f89a
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-28 16:34:05 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "dogeonK",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/dogeon.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using node.",
     email: "kimdogeon1@gmail.com",
