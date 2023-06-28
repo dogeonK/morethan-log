2ef0f00290e79301e012afc4f7a1fffef1d353ce
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-06-28 20:25:23 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -25,7 +25,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
