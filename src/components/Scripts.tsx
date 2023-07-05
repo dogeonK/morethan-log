30a29eb33315b426eee3528e793cad1bf5722d67
dogeonK <102578327+dogeonK@users.noreply.github.com>
2023-07-05 16:27:54 +0900

    Update Scripts.tsx

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -5,24 +5,15 @@ const Scripts: React.FC = () => (
   <>
     {CONFIG?.googleAnalytics?.enable === true && (
       <>
-        /*<Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}`}
-        />
-        <Script strategy="lazyOnload" id="ga">
+        <script src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}`} />
+        <script>
           {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
+            
             gtag('config', '${CONFIG.googleAnalytics.config.measurementId}', {
               page_path: window.location.pathname,
             });`}
-        </Script>*/
-        <script async src="https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${CONFIG.googleAnalytics.config.measurementId}');
         </script>
       </>
     )}
